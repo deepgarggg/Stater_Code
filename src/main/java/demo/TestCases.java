a350abb44c9dea7e510ce36e4e62cf4a6ac00785
Deep Garg <deepgarggg>
2024-01-11 13:34:05 +0530

    Your commit message

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -24,10 +25,34 @@ public class TestCases {
     }
 
     
-    public  void testCase01(){
+    public  void testCase01() throws InterruptedException{
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-top']")));
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-left']")));
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(),'LEFT')]")).getText());
+        driver.switchTo().parentFrame();
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-middle']")));
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//*[contains(@id,'content')]")).getText());
+        driver.switchTo().parentFrame();
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-right']")));
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(),'RIGHT')]")).getText());
+        driver.switchTo().parentFrame();
+        Thread.sleep(2000);
+        driver.switchTo().parentFrame();
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-bottom']")));
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(),'BOTTOM')]")).getText());
+        driver.switchTo().parentFrame();
+
+        System.out.println("end Test case: testCase01");
     }
 
 
